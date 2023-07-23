affe03269b9837e088952dff5927c53d7ef63931
Yann Sionneau <ysionneau@kalray.eu>
2023-07-23 13:31:48 +0200

    genrootfs.sh: allow to run it on host to generate a target rootfs
    target `busybox --list-full` cannot be run on host
    but the host one can be run, it should contain the same list of binaries.
    This is used to populate the rootfs with all the busybox symlinks.

--- scripts/genrootfs.sh
+++ scripts/genrootfs.sh
@@ -32,7 +32,7 @@ ${APK:-apk} add --keys-dir "$keys_dir" --no-cache \
 	--repositories-file "$repositories_file" \
 	--no-script --root "$tmp" --initdb --arch "$arch" \
 	"$@"
-for link in $("$tmp"/bin/busybox --list-full); do
+for link in $(/bin/busybox --list-full); do
 	[ -e "$tmp"/$link ] || ln -s /bin/busybox "$tmp"/$link
 done
 
